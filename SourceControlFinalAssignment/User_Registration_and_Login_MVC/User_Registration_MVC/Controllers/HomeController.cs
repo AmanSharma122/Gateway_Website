0ba883fe8f787d3d8d2639f65c5754a5422e6dc2
AmanSharma122 <amansharma1999feb@gmail.com>
2020-12-22 15:53:55 +0530

    Project Assignmnet Submission

---
+++ SourceControlFinalAssignment/User_Registration_and_Login_MVC/User_Registration_MVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace User_Registration_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            UsersEntities usersEntities = new UsersEntities();
+            usersEntities.Users.Add(user);
+            usersEntities.SaveChanges();
+            string message = string.Empty;
+            switch (user.UserId)
+            {
+                case -1:
+                    message = "Username already exists.\\nPlease choose a different username.";
+                    break;
+                case -2:
+                    message = "Supplied email address has already been used.";
+                    break;
+                default:
+                    message = "Registration successful.\\nUser Id: " + user.UserId.ToString();
+                    break;
+            }
+            ViewBag.Message = message;
+
+            return View(user);
+        }
+    }
+}
